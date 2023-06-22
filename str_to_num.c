d7787a078d0173148add59ee5867e07fdd57a1ea
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-06-22 16:19:11 +0300

    Create str_to_num.c

---
+++ str_to_num.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * str_to_num - convert str to number
+ * @str: The string to convert
+ *
+ * Return: Converted number
+ */
+int str_to_num(char *str)
+{
+	int result = 0;
+	int sign = 1;
+
+	if (str[0] == '-')
+	{
+		str++;
+		sign *= -1;
+	}
+
+	result = atoi(str) * sign;
+
+	return result;
+}
